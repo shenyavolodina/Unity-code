07991918074d8fa60845cc565efd4b71de1e6872
shenyavolodina <shenyavolodina@gmail.com>
2022-04-05 13:46:47 +0300

    Update InteractiveObject.cs

--- InteractionControllers/InteractiveObject.cs
+++ InteractionControllers/InteractiveObject.cs
@@ -4,16 +4,41 @@ using UnityEngine;
 
 namespace UnityIC
 {
-        public class InteractiveObject : MonoBehaviour
+         public class InteractiveObject : MonoBehaviour
         {
             public bool isInteractive = true;
-            public bool isGlobalLockInteractive = false;
+
+            public virtual void OnEnable()
+            {
+                GlobalSceneInteractionController.OnEnableSceneInteraction += SetInteractiveTrue;
+                GlobalSceneInteractionController.OnDisableSceneInteraction += SetInteractiveFalse;
+            }
+
+            public virtual void OnDisable()
+            {
+                GlobalSceneInteractionController.OnEnableSceneInteraction -= SetInteractiveTrue;
+                GlobalSceneInteractionController.OnDisableSceneInteraction -= SetInteractiveFalse;
+            }
 
             public void SetInteractive(bool flag)
             {
                 if (GlobalSceneInteractionController.isGlobalLockInteractive)
                     return;
                 isInteractive = flag;
+            }
+
+            public void SetInteractiveTrue()
+            {
+                if (GlobalSceneInteractionController.isGlobalLockInteractive)
+                    return;
+                isInteractive = true;
+            }
+
+            public void SetInteractiveFalse()
+            {
+                if (GlobalSceneInteractionController.isGlobalLockInteractive)
+                    return;
+                isInteractive = false;
             }
         }
 }
