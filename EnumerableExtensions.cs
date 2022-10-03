1726b8dced0b37b1eefb1096542253817c3894aa
shenyavolodina <shenyavolodina@gmail.com>
2022-10-03 20:39:04 +0300

    Add files via upload

---
+++ EnumerableExtensions.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System;
+
+namespace UnityIC
+{
+    public static class EnumerableExtensions
+    {
+        private static Random m_Random = new Random();
+        
+        public static IEnumerable<T> Randomize<T>(this IEnumerable<T> source)
+        {
+            List<T> tempList = source.ToList();
+
+            for (int i = 0; i < tempList.Count; i++)
+            {
+                int j = m_Random.Next(i, tempList.Count);
+                yield return tempList[j];
+
+                tempList[j] = tempList[i];
+            }
+        }
+
+        public static IEnumerable<T> Randomize<T>(this IEnumerable<T> source, T doLast)
+        {
+            source = source.Randomize();
+            List<T> tempList = source.ToList();
+
+            for (int i = 0; i < tempList.Count; i++)
+            { 
+                if(tempList[i].GetHashCode() != doLast.GetHashCode())
+                {
+                    yield return tempList[i];
+                }
+            }
+            yield return doLast;
+        }
+
+    }
+}
