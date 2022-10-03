26c14403910f259a904d4fd57f946d8f419723a2
shenyavolodina <shenyavolodina@gmail.com>
2022-10-03 20:31:19 +0300

    Update CharacterAnimatorController.cs

--- AnimationControllers/CharacterAnimatorController.cs
+++ AnimationControllers/CharacterAnimatorController.cs
@@ -8,29 +8,23 @@ namespace UnityIC
 {
     //The class for checking the end of an animation by calling the OnComplete method.
     //Example : characterAnimatorController.SetAnimation(AnimatorParameters.Idle).OnComplete(()=>Debug.Log("AnimationComplete"));
-    public class CharacterAnimatorController : MonoBehaviour
+      public class CharacterAnimatorController : MonoBehaviour
     {
         public AnimatorParameters parameter = AnimatorParameters.Idle;
         public AnimationCallback animationCallback = null;
 
-        private static CharacterAnimatorController m_characterAnimatorController = null;
+        private AnimatorParameters _prevParameter = AnimatorParameters.Idle;
         private Animator animator = null;
         private List<AnimatorClipInfo> _animatorClipInfos = new List<AnimatorClipInfo>();
         private float _cureentClipLength = 0f;
         private float _countOfClips = 0f;
-        private void Awake()
-        {
-            m_characterAnimatorController = this;
-        }
+
+        public event Action<AnimatorParameters> OnChangeAnimation = null;
 
         private void OnEnable()
         {
             animator = GetComponentInChildren<Animator>();
             SetAnimation(parameter);
-        }
-        public static void InvokeCallback()
-        {
-            m_characterAnimatorController.animationCallback?.Invoke();
         }
 
         private void Update()
@@ -44,19 +38,7 @@ namespace UnityIC
                 }
             }
         }
-
-        public CharacterAnimatorController SetAnimation(AnimatorParameters newParameter, int countOfClips = 1)
-        {
-            StopCoroutine(WaitForFinishingAnimation());
-            animationCallback = null;
-            animator.SetBool(parameter.ToString(), false);
-            _countOfClips = countOfClips;
-            parameter = newParameter;
-            animator.SetBool(parameter.ToString(), true);
-            StartCoroutine(WaitForFinishingAnimation());
-            return this;
-        }
-
+        
         private IEnumerator WaitForFinishingAnimation()
         {
             float _resultingLength = 0f;
@@ -66,12 +48,36 @@ namespace UnityIC
             {
                 _resultingLength = _cureentClipLength * _countOfClips;
                 yield return new WaitForSeconds(_resultingLength);
-                m_characterAnimatorController.OnComplete(animationCallback);
+                this.OnComplete(animationCallback);
                 InvokeCallback();
             }
         }
-    }
+        
+        public AnimatorParameters GetPrevParameter()
+        {
+            return _prevParameter;
+        }
 
+        public CharacterAnimatorController SetAnimation(AnimatorParameters newParameter, int countOfClips = 1)
+        {
+            _prevParameter = parameter;
+            OnChangeAnimation?.Invoke(newParameter);
+            StopCoroutine(WaitForFinishingAnimation());
+            animationCallback = null;
+            animator.SetBool(parameter.ToString(), false);
+            _countOfClips = countOfClips;
+            parameter = newParameter;
+            animator.SetBool(parameter.ToString(), true);
+            StartCoroutine(WaitForFinishingAnimation());
+            return this;
+        }
+        
+        public void InvokeCallback()
+        {
+            animationCallback?.Invoke();
+        }
+    }
+    
     public enum AnimatorParameters
     {
         None = 0,
